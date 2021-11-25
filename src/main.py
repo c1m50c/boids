2c8668eea28ad6ecc18d993a2843ab989b38ca99
c1m50c <terminalityms@gmail.com>
2021-11-25 16:29:50 -0600

    Drawing Boilerplate 🔥🍽

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
+import pygame
+
+# Colors
+BACKGROUND_COLOR = (26, 26, 26)
+
+screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+
+
 def main():
-    pass
+    global screen
+    global clock
+    
+    pygame.display.set_caption("Boids")
+    
+    while True:
+        width, height = screen.get_size()
+        screen.fill(BACKGROUND_COLOR)
+        
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            return
+        
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
